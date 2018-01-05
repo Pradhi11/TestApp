04cd4d8e98648d7d8744e032b09b12147218af9f
Pradeep <pradhi.sjv@gmail.com>
2018-01-05 12:23:38 +0530

    from 2nd branch update

--- app/src/main/java/test/com/testapp/MainActivity.java
+++ app/src/main/java/test/com/testapp/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test","test");
+        Log.d("test","update from testAppbranch2Develop");
+
     }
 }
